dd9e958ad756da00bde0cc4cd2280c012b13935f
Santiago Firpo <santiagofirpo4@gmail.com>
2024-11-20 22:33:38 -0300

    Used arrow functions to avoid weirdness with this

--- src/tests/usingMockingContext.ts
+++ src/tests/usingMockingContext.ts
@@ -1,9 +1,9 @@
 import { using, usingAsync } from "~/tests/using";
-import { FileMockingContext } from "~/tests/fileMocking";
+import { type FileMockingContext } from "~/tests/fileMocking";
 
 export function usingFileMockerAsync(context: FileMockingContext, func: () => Promise<void>) {
-  return usingAsync(context, func, context.cleanup);
+  return usingAsync(context, func, () => context.cleanup());
 }
 export function usingFileMocker(context: FileMockingContext, func: () => void) {
-  return using(context, func, context.cleanup);
+  return using(context, func, () => context.cleanup());
 }
