0c8d6b4d49ef999e4aa99f6ae09e6e34ebd6ae99
Santiago Firpo <santiagofirpo4@gmail.com>
2024-11-20 22:32:52 -0300

    Added bundling support with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+﻿import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/typefs.ts"],
+  outDir: "dist",
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});
