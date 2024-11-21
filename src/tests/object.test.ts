fe51dee0482aca2d3f5cf670ffd9d5e34edf3f5d
Santiago Firpo <santiagofirpo4@gmail.com>
2024-11-21 18:13:01 -0300

    Added failure tests for object

--- src/tests/object.test.ts
+++ src/tests/object.test.ts
@@ -3,6 +3,7 @@ import { createFileMocker } from "~/tests/shared/mocking/fileMocking";
 import { usingFileMockerAsync } from "~/tests/shared/mocking/useFileMocker";
 import { expect, test } from "vitest";
 import {typefs} from "~/schemas";
+import { couldNotReadDirectory } from "~/types";
 
 test("The object schema should parse an object", async () => {
   const fileMocker = createFileMocker(toPath("test-resources/object/objectTest"))
@@ -27,5 +28,56 @@ test("The object schema should parse an object", async () => {
     expect(objectResult.okValue.markdown.path).toBe(safeJoin(fileMocker.getCurrentDirectory(), toPath("b.md")));
     expect(objectResult.okValue.image.name).toBe("c");
     expect(objectResult.okValue.image.url).toBe("/object/objectTest/c.jpg");
+  });
+});
+
+test("The object schema should fail if the directory does not exist", async () => {
+  const objectResult = await typefs.object({
+    url: typefs.url(),
+    markdown: typefs.markdown(),
+    image: typefs.image("test-resources")
+  }).parse(toPath("test-resources/object/doesNotExist"));
+
+  if (objectResult.wasResultSuccessful) {
+    throw new Error("Expected error");
+  }
+
+  expect(objectResult.errorValue).toBe(couldNotReadDirectory);
+});
+
+test("The object schema should fail if the directory is not a directory", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/object/objectTest"))
+    .createFile(toPath("notADirectory.txt"), "Hello World");
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const objectResult = await typefs.object({
+      url: typefs.url(),
+      markdown: typefs.markdown(),
+      image: typefs.image("test-resources")
+    }).parse(fileMocker.getCurrentFile());
+    if (objectResult.wasResultSuccessful) {
+      throw new Error("Expected error");
+    }
+
+    expect(objectResult.errorValue).toBe("could not read directory");
+  });
+});
+
+test("The object schema should fail if the directory does not contain all the required files", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/object/objectTest"))
+    .createFile(toPath("a.url"), "https://www.google.com")
+    .createFile(toPath("b.md"), "# Hello World");
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const objectResult = await typefs.object({
+      url: typefs.url(),
+      markdown: typefs.markdown(),
+      image: typefs.image("test-resources")
+    }).parse(fileMocker.getCurrentDirectory());
+    if (objectResult.wasResultSuccessful) {
+      throw new Error("Expected error");
+    }
+
+    expect(objectResult.errorValue).toBe("no matches");
   });
 });
