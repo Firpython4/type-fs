6c69c1b469aac3e60ac01d4b74c130f6a9ad3934
BrAvellar <b.avellar01@edu.pucrs.br>
2024-11-21 21:49:47 -0300

    Added new tests for image schema based on markdown tests

--- src/tests/image.test.ts
+++ src/tests/image.test.ts
@@ -1,6 +1,6 @@
 import { createFileMocker } from "~/tests/shared/mocking/fileMocking";
 import { toPath } from "~/fileManagement";
-import { expect, test } from "vitest";
+import { expect, test, vitest } from "vitest";
 import { usingFileMockerAsync } from "~/tests/shared/mocking/useFileMocker";
 import {typefs} from "~/schemas";
 
@@ -43,4 +43,79 @@ test("The image schema should fail if the file is not an image", async () => {
 
     expect(imageResult.errorValue).toBe("invalid extension");
   });
+});
+
+test("A image schema with a name should parse a image file with the given name", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/image/imageTest"))
+    .copyFile(toPath("test-resources/gratisography-cool-cat.jpg"), toPath("gratisography-cool-cat.jpg"));
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const imageResult = await typefs.image().withName("gratisography-cool-cat").parse(fileMocker.getCurrentFile());
+    expect(imageResult.wasResultSuccessful).toBeTruthy();
+    if (!imageResult.wasResultSuccessful) {
+      throw new Error(imageResult.errorValue);
+    }
+    expect(imageResult.okValue.name).toBe("gratisography-cool-cat");
+  });
+});
+
+test("A image schema with a name should fail if the file does not match the name", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/image/imageTest"))
+    .copyFile(toPath("test-resources/gratisography-cool-cat.jpg"), toPath("gratisography-cool-cat.jpg"));
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const imageResult = await typefs.image().withName("wrong-name").parse(fileMocker.getCurrentFile());
+    if (imageResult.wasResultSuccessful) {
+      throw new Error("Expected error");
+    }
+    expect(imageResult.errorValue).toBe("name does not match");
+  });
+});
+
+test("A image schema with a name should fail if the file does not match the name", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/image/imageTest"))
+    .copyFile(toPath("test-resources/gratisography-cool-cat.jpg"), toPath("gratisography-cool-cat.jpg"));
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const imageResult = await typefs.image().withName("wrong-name").parse(fileMocker.getCurrentFile());
+    if (imageResult.wasResultSuccessful) {
+      throw new Error("Expected error");
+    }
+    expect(imageResult.errorValue).toBe("name does not match");
+  });
+});
+
+test("A image schema with an error handler should parse a image file with the given name", async () => {
+  const fileMocker = createFileMocker(toPath("test-resources/image/imageTest"))
+    .copyFile(toPath("test-resources/gratisography-cool-cat.jpg"), toPath("gratisography-cool-cat.jpg"));
+
+
+  await usingFileMockerAsync(fileMocker, async () => {
+    const spy = vitest.fn();
+    const imageResult = await typefs.image()
+      .withErrorHandler(spy)
+      .withName("gratisography-cool-cat")
+      .parse(fileMocker.getCurrentFile());
+    expect(imageResult.wasResultSuccessful).toBeTruthy();
+    if (!imageResult.wasResultSuccessful) {
+      throw new Error("Expected error");
+    }
+    expect(imageResult.okValue.name).toBe("gratisography-cool-cat");
+    expect(spy).not.toHaveBeenCalled();
+  });
+});
+
+test("A image schema with an error handler should fail and call the error handler if the file does not exist", async () => {
+  let i = 0;
+  const spy = vitest.fn();
+  const imageResult = await typefs.image()
+    .withName("gratisography-cool-cat")
+    .withErrorHandler(spy)
+    .parse(toPath("test-resources/image/doesNotExist/test.jpg"));
+
+  if (imageResult.wasResultSuccessful) {
+    throw new Error("Expected error");
+  }
+
+  expect(spy).toHaveBeenCalled();
 });
