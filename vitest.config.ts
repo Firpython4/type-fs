295fd7b3b85049e388626aec7163f70e3f1c7c25
Santiago Firpo <santiagofirpo4@gmail.com>
2024-11-26 16:24:25 -0300

    Added coverage report support

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      exclude: ["src/examples/**", "dist/**", "eslint.config.js", "tsup.config.ts", "vitest.config.ts", "src/tests/**"],
+    },
+  }
 })
